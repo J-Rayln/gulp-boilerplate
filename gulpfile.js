83f31f9c2beacdbfd8cee067a95b29d827c28987
J-Rayln <103427109+J-Rayln@users.noreply.github.com>
2022-07-16 22:02:16 -0700

    Removed gulp-replace functionality.  Closes #2

--- gulpfile.js
+++ gulpfile.js
@@ -10,104 +10,102 @@ const purgecss = require("gulp-purgecss");
 const concat = require("gulp-concat");
 const terser = require("gulp-terser");
 // Other
-// TODO #2 Add cache busting function or delete this package
-const replace = require("gulp-replace");
 const header = require("gulp-header");
 const browsersync = require("browser-sync").create();
 
 // Establish file paths
 const paths = {
-    css: {
-        src: "./src/scss/**/*.scss",
-        dest: "./assets/css",
-        destView: "./_view",
-    },
-    js: {
-        src: "./src/js/**/*.js",
-        dest: "./assets/js",
-    },
-    html: {
-        src: "./*.html",
-        serve: "./",
-    },
+  css: {
+    src: "./src/scss/**/*.scss",
+    dest: "./assets/css",
+    destView: "./_view",
+  },
+  js: {
+    src: "./src/js/**/*.js",
+    dest: "./assets/js",
+  },
+  html: {
+    src: "./*.html",
+    serve: "./",
+  },
 };
 
 // Header constants
 const pkg = require("./package.json");
 const banner = [
-    "/**",
-    " * <%= pkg.name %> - <%= pkg.description %>",
-    " * @version v<%= pkg.version %>",
-    " * @license <%= pkg.license %>",
-    " */",
-    "",
+  "/**",
+  " * <%= pkg.name %> - <%= pkg.description %>",
+  " * @version v<%= pkg.version %>",
+  " * @license <%= pkg.license %>",
+  " */",
+  "",
 ].join("\n");
 
 // Complies & minifies CSS
 function scssTask() {
-    return src(paths.css.src, { sourcemaps: true })
-        .pipe(scss()) // complie the SCSS files
-        .pipe(purgecss({ content: ["*.html"] })) // purge any unused CSS classes (like utiltites)
-        .pipe(postcss([autoprefixer(), cssnano()])) // autoprefix and minify the CSS tha'ts left
-        .pipe(header(banner, { pkg: pkg })) // insert the banner at the top of the file
-        .pipe(dest(paths.css.dest, { sourcemaps: "." })); // save the files where we want them
+  return src(paths.css.src, { sourcemaps: true })
+    .pipe(scss()) // complie the SCSS files
+    .pipe(purgecss({ content: ["*.html"] })) // purge any unused CSS classes (like utiltites)
+    .pipe(postcss([autoprefixer(), cssnano()])) // autoprefix and minify the CSS tha'ts left
+    .pipe(header(banner, { pkg: pkg })) // insert the banner at the top of the file
+    .pipe(dest(paths.css.dest, { sourcemaps: "." })); // save the files where we want them
 }
 
 // Complies CSS into uncompressed css file
 // Used for easy viewing during development.
 function scssViewtask() {
-    return src(paths.css.src, { sourcemaps: false })
-        .pipe(scss())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(dest(paths.css.destView));
+  return src(paths.css.src, { sourcemaps: false })
+    .pipe(scss())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(dest(paths.css.destView));
 }
 
 // Cooncatenates and uglifies JavsScript files
 function jsTask() {
-    return src(paths.js.src, { sourcemaps: true })
-        .pipe(concat("scripts.js"))
-        .pipe(terser())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(dest(paths.js.dest, { sourcemaps: "." }));
+  return src(paths.js.src, { sourcemaps: true })
+    .pipe(concat("scripts.js"))
+    .pipe(terser())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(dest(paths.js.dest, { sourcemaps: "." }));
 }
 
 // BrowserSync to create local server
 function browserSyncServe(cb) {
-    browsersync.init({
-        server: {
-            baseDir: paths.html.serve,
-        },
-        notify: {
-            styles: {
-                top: "auto",
-                bottom: "0",
-            },
-        },
-    });
-    cb();
+  browsersync.init({
+    server: {
+      baseDir: paths.html.serve,
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0",
+      },
+    },
+  });
+  cb();
 }
 
 // Reloads BrowserSync upon change to JS or SCSS files
 function browserSyncReload(cb) {
-    browsersync.reload();
-    cb();
+  browsersync.reload();
+  cb();
 }
 
 // Watches CSS, JS, and HTML files for changes and rebuilds on file save
 function watchTask() {
-    watch(
-        [paths.css.src, paths.js.src, paths.html.src],
-        series(parallel(scssTask, scssViewtask, jsTask), browserSyncReload)
-    );
+  watch(
+    [paths.css.src, paths.js.src, paths.html.src],
+    series(parallel(scssTask, scssViewtask, jsTask), browserSyncReload)
+  );
 }
 
 // Watchs CSS, JS, and HTML files for changes and rebuilds on file save
 // This task does NOT init or reload BrowserSync
 function watchNoBsTask() {
-    watch(
-        [paths.css.src, paths.js.src, paths.html.src],
-        series(parallel(scssTask, scssViewtask, jsTask))
-    );
+  watch(
+    [paths.css.src, paths.js.src, paths.html.src],
+    series(parallel(scssTask, scssViewtask, jsTask))
+  );
 }
 
 // Default Gulp task
@@ -115,9 +113,9 @@ function watchNoBsTask() {
 // 2nd, spins up a BrowserSync server
 // 3rd, runs the watchTask
 exports.default = series(
-    parallel(scssTask, scssViewtask, jsTask),
-    browserSyncServe,
-    watchTask
+  parallel(scssTask, scssViewtask, jsTask),
+  browserSyncServe,
+  watchTask
 );
 
 // Build & Watch Only
