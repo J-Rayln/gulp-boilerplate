0633c2472670de763ee2938f43078493e284e104
J-Rayln <103427109+J-Rayln@users.noreply.github.com>
2022-07-17 22:22:46 -0700

    Gulp workflow updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,102 @@
-// Initialize modules
-const { src, dest, watch, series, parallel } = require("gulp");
-// Styles
-const scss = require("gulp-sass")(require("sass"));
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
-const purgecss = require("gulp-purgecss");
-// JavaScript
-const concat = require("gulp-concat");
-const terser = require("gulp-terser");
-// Other
-const header = require("gulp-header");
-const browsersync = require("browser-sync").create();
+// initialize modules
+const { src, dest, watch, parallel, series } = require("gulp");
 
-// Establish file paths
-const paths = {
-  css: {
-    src: "./src/scss/**/*.scss",
-    dest: "./assets/css",
-    destView: "./_view",
+const autoprefixer = require("autoprefixer");
+const browserSync = require("browser-sync");
+const cssnano = require("cssnano");
+const concat = require("gulp-concat");
+const del = require("del");
+const header = require("gulp-header");
+const gulpif = require("gulp-if");
+const postcss = require("gulp-postcss");
+const purgecss = require("gulp-purgecss");
+const rename = require("gulp-rename");
+const replace = require("gulp-replace");
+const sass = require("gulp-sass")(require("sass"));
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+const args = require("yargs").argv;
+
+// set environment variable
+const env = args.env ? process.env.NODE_ENV : "development";
+
+// file path variables
+const files = {
+  styles: {
+    src: "src/scss/**/*.scss",
+    dest: "dist/css",
   },
   js: {
-    src: "./src/js/**/*.js",
-    dest: "./assets/js",
+    src: "src/js/**/*.js",
+    dest: "dist/js",
+  },
+  img: {
+    src: "src/img/**/*",
+    dest: "dist/img",
   },
   html: {
-    src: "./*.html",
     serve: "./",
   },
 };
 
-// Header constants
+// banner/header constants
 const pkg = require("./package.json");
 const banner = [
-  "/**",
-  " * <%= pkg.name %> - <%= pkg.description %>",
-  " * @version v<%= pkg.version %>",
-  " * @license <%= pkg.license %>",
+  "/*",
+  " <%= pkg.name %> - <%= pkg.description %>",
+  " @version v<%= pkg.version %>",
+  " @license <%= pkg.license %>",
   " */",
   "",
 ].join("\n");
 
-// Complies & minifies CSS
+// sass task
 function scssTask() {
-  return src(paths.css.src, { sourcemaps: true })
-    .pipe(scss()) // complie the SCSS files
-    .pipe(purgecss({ content: ["*.html"] })) // purge any unused CSS classes (like utiltites)
-    .pipe(postcss([autoprefixer(), cssnano()])) // autoprefix and minify the CSS tha'ts left
-    .pipe(header(banner, { pkg: pkg })) // insert the banner at the top of the file
-    .pipe(dest(paths.css.dest, { sourcemaps: "." })); // save the files where we want them
+  return (
+    src(files.styles.src)
+      // only build sourcemaps during development
+      .pipe(gulpif(args.env != "production", sourcemaps.init()))
+      .pipe(sass())
+      // purge an unused CSS classes during production build
+      .pipe(
+        gulpif(args.env === "production", purgecss({ content: ["*.html"] }))
+      )
+      // only autoprefix and minify in production
+      .pipe(
+        gulpif(args.env === "production", postcss([autoprefixer(), cssnano()]))
+      )
+      // write suffix to *.min.css during production
+      // .pipe(gulpif(args.env === "production", rename({ suffix: ".min" })))
+      // only build sourcemaps during development
+      .pipe(header(banner, { pkg: pkg }))
+      .pipe(gulpif(args.env != "production", sourcemaps.write(".")))
+      .pipe(dest(files.styles.dest))
+  );
 }
 
-// Complies CSS into uncompressed css file
-// Used for easy viewing during development.
-function scssViewtask() {
-  return src(paths.css.src, { sourcemaps: false })
-    .pipe(scss())
-    .pipe(header(banner, { pkg: pkg }))
-    .pipe(dest(paths.css.destView));
-}
-
-// Cooncatenates and uglifies JavsScript files
+// javascript task
 function jsTask() {
-  return src(paths.js.src, { sourcemaps: true })
+  return src(files.js.src)
     .pipe(concat("scripts.js"))
-    .pipe(terser())
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
     .pipe(header(banner, { pkg: pkg }))
-    .pipe(dest(paths.js.dest, { sourcemaps: "." }));
+    .pipe(dest(files.js.dest));
+}
+
+// cachebusting task
+const cbString = new Date().getTime();
+function cacheBustingTask() {
+  return src(["index.html"])
+    .pipe(replace(/v=\d+/g, "v=" + cbString))
+    .pipe(dest("."));
 }
 
 // BrowserSync to create local server
 function browserSyncServe(cb) {
-  browsersync.init({
+  browserSync.init({
     server: {
-      baseDir: paths.html.serve,
+      baseDir: files.html.serve,
     },
     notify: {
       styles: {
@@ -87,37 +110,44 @@ function browserSyncServe(cb) {
 
 // Reloads BrowserSync upon change to JS or SCSS files
 function browserSyncReload(cb) {
-  browsersync.reload();
+  browserSync.reload();
   cb();
 }
 
-// Watches CSS, JS, and HTML files for changes and rebuilds on file save
+// watch task
 function watchTask() {
   watch(
-    [paths.css.src, paths.js.src, paths.html.src],
-    series(parallel(scssTask, scssViewtask, jsTask), browserSyncReload)
+    [files.styles.src, files.js.src],
+    series(
+      cleanTask,
+      parallel(scssTask, jsTask),
+      cacheBustingTask,
+      browserSyncReload
+    )
   );
 }
 
-// Watchs CSS, JS, and HTML files for changes and rebuilds on file save
-// This task does NOT init or reload BrowserSync
-function watchNoBsTask() {
-  watch(
-    [paths.css.src, paths.js.src, paths.html.src],
-    series(parallel(scssTask, scssViewtask, jsTask))
-  );
+// clean task
+function cleanTask() {
+  return del("./dist");
 }
 
-// Default Gulp task
-// 1st, runs scssTask, scssViewTask, and jsTask simultaneously
-// 2nd, spins up a BrowserSync server
-// 3rd, runs the watchTask
+// default task
 exports.default = series(
-  parallel(scssTask, scssViewtask, jsTask),
+  cleanTask,
+  parallel(scssTask, jsTask),
+  cacheBustingTask,
   browserSyncServe,
   watchTask
 );
 
-// Build & Watch Only
-// This task does not initialize or reload a BrowserSync server
-exports.nobs = series(parallel(scssTask, scssViewtask, jsTask), watchNoBsTask);
+exports.devNoBs = series(
+  cleanTask,
+  parallel(scssTask, jsTask),
+  cacheBustingTask,
+  watchTask
+);
+
+exports.build = series(cleanTask, parallel(scssTask, jsTask), cacheBustingTask);
+
+exports.clean = cleanTask;
